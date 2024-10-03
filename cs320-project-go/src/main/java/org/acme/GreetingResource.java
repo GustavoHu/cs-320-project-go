a9b719e8f074fdc0c193c223e8da1234e356cd44
GustavoHu <166774175+GustavoHu@users.noreply.github.com>
2024-10-03 11:47:08 -0600

    assingment

--- cs320-project-go/src/main/java/org/acme/GreetingResource.java
+++ cs320-project-go/src/main/java/org/acme/GreetingResource.java
@@ -1,11 +1,10 @@
 package org.acme;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/personalized/{name}")
+
+@Path("/hello")
 public class GreetingResource {
 
     @GET
@@ -15,9 +14,29 @@ public class GreetingResource {
     }
 
     @Path("/personalized/{name}")
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String personalizedHello(@PathParam("name") String name) {
         return "Hello " + name;
     }
+
+    @Path("/personalized")
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public String personalizedHelloPost(Person p) {
+        return "Hello " + p.getFirst() + " " + p.getLast();
+    }
+
+    public static class Person {
+        private String first;
+        private String last;
+
+        public String getFirst() { return first; }
+        public void setFirstName(String first) { this.first = first; }
+        public String getLast() { return last; }
+        public void setLastName(String last) { this.last = last; }
+}
+
 }
