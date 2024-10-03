42a5810955466473ba923ed93f71c0af960325b9
GustavoHu <166774175+GustavoHu@users.noreply.github.com>
2024-10-03 11:41:07 -0600

    assingment

--- cs320-project-go/src/main/java/org/acme/GreetingResource.java
+++ cs320-project-go/src/main/java/org/acme/GreetingResource.java
@@ -5,12 +5,19 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/personalized/{name}")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello RESTEasy";
+    }
+
+    @Path("/personalized/{name}")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String personalizedHello(@PathParam("name") String name) {
+        return "Hello " + name;
     }
 }
